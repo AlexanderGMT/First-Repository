ce41f1ac7d18327618f23460137b378ec607b6cd
Tejeda Gomez <ac8400@student.jamk.fi>
2022-09-26 09:12:40 +0300

    Ejercicio05

---
+++ Sesion05/ejercicio/Ejercicio05.js
@@ -0,0 +1,17 @@
+let altura = 177;
+
+let altura2 = 1.77
+
+let peso = 75.5
+
+let alturafixed = altura2.toFixed(1)
+console.log(alturafixed)
+
+let pesoabajo = Math.floor(75.5)
+console.log(pesoabajo)
+
+let max_min = (Number.MAX_VALUE + 1 === Number.MIN_VALUE)
+console.log(max_min) // false
+
+
+
